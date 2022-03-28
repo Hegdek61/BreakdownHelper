b3b89bedbf4160803e956f17c111bb72097b49cd
Keerthi <hegdek61@gmail.com>
2022-03-28 14:09:14 +0530

    added registation/login design

--- app/src/main/java/uk/ac/tees/aad/B1212361/LoginActivity.java
+++ app/src/main/java/uk/ac/tees/aad/B1212361/LoginActivity.java
@@ -2,7 +2,10 @@ package uk.ac.tees.aad.B1212361;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button create = findViewById(R.id.createAccount);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
     }
 }
