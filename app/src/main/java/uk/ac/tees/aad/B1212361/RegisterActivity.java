8cb23db7b44783f0789adaf48ebd97c8b05dea64
Keerthi <hegdek61@gmail.com>
2022-04-01 00:28:34 +0530

    update on login

--- app/src/main/java/uk/ac/tees/aad/B1212361/RegisterActivity.java
+++ app/src/main/java/uk/ac/tees/aad/B1212361/RegisterActivity.java
@@ -2,13 +2,25 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button logon = findViewById(R.id.login_btn_sign);
+
+        logon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 }
