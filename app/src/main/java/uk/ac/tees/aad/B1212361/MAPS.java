91b7e2df884bf27148464bc8e605381934354e53
Keerthi <hegdek61@gmail.com>
2022-04-11 13:39:55 +0530

    added MAPS

---
+++ app/src/main/java/uk/ac/tees/aad/B1212361/MAPS.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.B1212361;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAPS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
